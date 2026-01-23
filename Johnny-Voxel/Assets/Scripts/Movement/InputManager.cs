e67a510ea1488dae2df8eda63c8c0288e6c5e510
santiagoalonso-ui <santiago.alonso@uniat.edu.mx>
2026-01-22 23:56:53 -0600

    Camara Isometrica funcionando

--- Johnny-Voxel/Assets/Scripts/Movement/InputManager.cs
+++ Johnny-Voxel/Assets/Scripts/Movement/InputManager.cs
@@ -1,12 +1,20 @@
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
     PlayerCtrl playerControls;
+    AnimatorManager animatorManager;
 
     public Vector2 movementInput;
+    private float moveAmount;
     public float verticalInput;
     public float horizontalInput;
+
+    private void Awake()
+    {
+        animatorManager = GetComponent<AnimatorManager>();
+    }
 
     private void OnEnable()
     {
@@ -36,5 +44,7 @@ public class InputManager : MonoBehaviour
     {
         verticalInput = movementInput.y;
         horizontalInput = movementInput.x;
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animatorManager.UpdateAnimatorValues(0, moveAmount);
     }
 }
